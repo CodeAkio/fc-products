faf725f9ea45774bcae6f7c7cc447b5cfd0bbf8c
Victor Siqueira <siqueira_2004@hotmail.com>
2024-04-08 15:00:33 -0300

    feat: add create product handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/CodeAkio/fc-products/configs"
+import (
+	"github.com/CodeAkio/fc-products/configs"
+	"github.com/CodeAkio/fc-products/internal/entity"
+	"github.com/CodeAkio/fc-products/internal/infra/database"
+	"github.com/CodeAkio/fc-products/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8000", nil)
 }
